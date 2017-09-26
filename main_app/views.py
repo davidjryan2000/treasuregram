af59ee9b6d9178be4393d40e69a4906073e37006
David Ryan <davidjryan2000@gmail.com>
2017-09-27 00:08:34 +0100

    movetreasure class to  models.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
+from .models import Treasure
 
 # Create your views here.
 def index(request):
+    treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures':treasures})
 
 
-class Treasure:
+'''class Treasure:
     def __init__(self, name,value,material,location,img_url):
         self.name = name
         self.value = value
@@ -18,4 +19,4 @@ treasures = [
     Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM",'http://courseware.codeschool.com/try_django/images/treasuregram-gold-nugget.png' ),
     Treasure("Fool's Gold", 0,'pyrite', "Fool's Falls, CO",'http://courseware.codeschool.com/try_django/images/treasuregram-fools-gold.png '),
     Treasure('Coffee Can',20.00,'tin','Acme, CA','http://courseware.codeschool.com/try_django/images/treasuregram-coffee-can.png ')
-]
+]'''
