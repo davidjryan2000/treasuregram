a72a43b9cb539afea20b611f746417209f29facc
David Ryan <davidjryan2000@gmail.com>
2017-10-04 11:53:46 +0100

    user image input fix

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Treasure(models.Model):
@@ -6,8 +7,7 @@ class Treasure(models.Model):
     value = models.DecimalField(max_digits=10,decimal_places=2)
     material = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='treasure_images',
-                            default='media/default.png')
+    image = models.ImageField(upload_to='treasure_images',default='media/default.png')
 
     def __str__(self):
         return self.name
