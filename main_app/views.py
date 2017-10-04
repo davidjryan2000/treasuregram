56cf48ec46f86292aa3493c78f53f98d57ca1564
David Ryan <davidjryan2000@gmail.com>
2017-10-04 12:42:39 +0100

    create user profile

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Treasure
 from .forms import TreasureForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -15,7 +16,15 @@ def detail(request, treasure_id):
 
 def post_treasure(request):
     form = TreasureForm(request.POST, request.FILES)
+    treasure = form.save(commit = False)
+    treasure.user=request.user
+    treasure.save()
     if form.is_valid():
         form.save(commit=True)
      
     return HttpResponseRedirect('/')
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    treasures = Treasure.objects.filter(user=user)
+    return render(request,'profile.html', {'username':username,'treasures': treasures})
